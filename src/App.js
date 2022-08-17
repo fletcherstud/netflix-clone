4f006196b12ae18fe3072dc46ef0a53137c25b78
Jonathan Fletcher <johnfletcher831@gmail.com>
2022-08-16 22:48:34 -0600

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
 
 function App() {
+  const user = null;
+
   return (
-    <div className="app">
-      <HomeScreen />
+    <div className="App">
+      <BrowserRouter>
+        {!user ? (
+          <Login />
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/screens/Login.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Login.css";
+function Login() {
+  return (
+    <div className="loginScreen">
+      <div className="loginScreen_background">
+        <img
+          className="loginScreen_logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="background"
+        />
+        <button className="loginScreen_button">Sign In</button>
+        <div className="loginScreen_gradient" />
+      </div>
+      <div className="loginScreen_body">
+        <>
+          <h1>Unlimted fils, TV programmes and more.</h1>
+          <h2>Watch anywhere. Cancel at any time.</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership
+          </h3>
+          <div className="loginScreen_input">
+            <form>
+              <input type="email" placeholder="Email Address" />
+              <button className="loginScreen_GetStarted">GET STARTED</button>
+            </form>
+          </div>
+        </>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
